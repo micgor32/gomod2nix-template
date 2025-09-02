738df88dd862f23a95d7f1821a47114f4a9b7e9f
Michal Gorlas <michal.gorlas@9elements.com>
2025-09-02 22:26:46 +0200

    fix: paths

--- templates/uroot/flake.nix
+++ templates/uroot/flake.nix
@@ -3,7 +3,7 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.gomod2nix.url = "github:micgor32/nixworkshop/golang-flake-template";
+  inputs.gomod2nix.url = "github:micgor32/nixworkshop?dir=golang-flake-template";
   inputs.gomod2nix.inputs.nixpkgs.follows = "nixpkgs";
   inputs.gomod2nix.inputs.flake-utils.follows = "flake-utils";
 
